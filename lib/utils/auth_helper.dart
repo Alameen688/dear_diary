de66d081f8a9dd6def6b17981f6e7feac21ea2a8
Alameen688 <ogundiran12@gmail.com>
2019-09-10 00:28:00 +0100

    Add auth helper

---
+++ lib/utils/auth_helper.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum AuthStatus { LOGGED_IN, LOGGED_OUT, IS_LOADING }
+
+class AuthHelper {
+  static saveInfo(token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('auth-token', token);
+  }
+
+  static isAuth() async {
+    final token = await getUserToken();
+    return token.isNotEmpty ? AuthStatus.LOGGED_IN : AuthStatus.LOGGED_OUT;
+  }
+
+  static getUserToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('auth-token') ?? '';
+  }
+}
