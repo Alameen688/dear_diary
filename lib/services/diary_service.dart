37d4a1625a07faeb6e6b7a61c0dc768769e070bc
Alameen688 <ogundiran12@gmail.com>
2019-09-10 00:22:47 +0100

    Add diary service for api communication

--- lib/services/diary_service.dart
+++ lib/services/diary_service.dart
@@ -1,10 +1,28 @@
-import 'package:http/http.dart' show Client;
+import 'package:dear_diary/utils/api_helper.dart';
+import 'package:dear_diary/utils/auth_helper.dart';
+import 'package:http/http.dart' show Client, Response;
 
 class DiaryService {
+  // TODO: Use DI to provide token, since it will be used for virtually all calls here
   final Client apiClient = Client();
-  static const _baseUrl = 'http://mydiaryoxygen.herokuapp.com/api/v1';
+  static const _baseUrl = BASE_URL;
 
-  getEntries() {
-    apiClient.get(_baseUrl);
+  Future<Response> getEntries() async {
+    final String url = '$_baseUrl/entries';
+    final String token = await AuthHelper.getUserToken();
+    return apiClient.get(url, headers: {
+      'Authorization': 'Bearer $token',
+    });
+  }
+
+  Future<Response> addEntry(Map<String, String> entry) async {
+    final String url = '$_baseUrl/entries';
+    final String token = await AuthHelper.getUserToken();
+    return apiClient.post(url, headers: {
+      'Authorization': 'Bearer $token',
+    }, body: {
+      'title': entry['title'],
+      'content': entry['content']
+    });
   }
 }
