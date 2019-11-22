a732088ef22efe47148cd0393bc9109a8c67125b
Alameen688 <ogundiran12@gmail.com>
2019-11-22 01:12:25 +0100

    Add user model for serializing user profile info

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,13 @@
 class User {
+  int id;
+  String fullName;
+  String content;
+  String email;
+  String entryCount;
 
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        fullName = json['fullname'],
+        email = json['email'],
+        entryCount = json['entry_count'];
 }
